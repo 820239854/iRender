21c7c0720beb7cda444be76346b174d13330cae3
Archer <820239854@qq.com>
2021-12-19 22:59:32 +0800

    Time stamp using delay

--- src/main.c
+++ src/main.c
@@ -74,7 +74,13 @@ vec2_t project(vec3_t point) {
 }
 
 void update(void) {
-    while (!SDL_TICKS_PASSED(SDL_GetTicks(), previous_frame_time + FRAME_TARGET_TIME));
+    // Wait some time until the reach the target frame time in milliseconds
+    int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks() - previous_frame_time);
+
+    // Only delay execution if we are running too fast
+    if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME) {
+        SDL_Delay(time_to_wait);
+    }
 
     previous_frame_time = SDL_GetTicks();
 
