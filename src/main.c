15b0a0088bbcaa0768e46c0694ef4e860650c195
Archer <820239854@qq.com>
2021-12-19 22:58:38 +0800

    Fix time Stamp

--- src/main.c
+++ src/main.c
@@ -18,6 +18,7 @@ vec3_t cube_rotation = { .x = 0, .y = 0, .z = 0 };
 float fov_factor = 640;
 
 bool is_running = false;
+int previous_frame_time = 0;
 
 void setup(void) {
     // Allocate the required memory in bytes to hold the color buffer
@@ -73,6 +74,10 @@ vec2_t project(vec3_t point) {
 }
 
 void update(void) {
+    while (!SDL_TICKS_PASSED(SDL_GetTicks(), previous_frame_time + FRAME_TARGET_TIME));
+
+    previous_frame_time = SDL_GetTicks();
+
     cube_rotation.x += 0.01;
     cube_rotation.y += 0.01;
     cube_rotation.z += 0.01;
